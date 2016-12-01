778ca98b49d8d5181e2381c58aad9cc0dfab42aa
Jiaduo <1183557534@qq.com>
2016-12-01 17:22:01 +0800

    update

--- fixmount.sh
+++ fixmount.sh
@@ -3,20 +3,25 @@
 #run as root!
 if [ "$#" -ne 2 ]
 then
-	echo Usage:error parm!
-	echo Usage:sudo ./fixmount.sh {mount_image} {mount_dir}
+	echo Usage:error parm!.
 	exit 1
 fi
 
 if [ ! -f "$1" ]
 then
-	echo file $1 not found!
+	echo File $1 not found!.
 	exit 1
 fi
 
 if [ ! -d "$2" ]
 then
 	mkdir $2
+else
+	files=`ls $2`
+	if [ ! -z "$files" ]; then
+		echo "Folder $2 is already mounted(not empty)."
+		exit 1
+	fi	
 fi
 
 block_size=4096
@@ -29,13 +34,13 @@ mount_file=$1.mount.fixed
 
 if [ ! -f "$mount_file" ]
 then
-	echo Creating \'$mount_file\' 
+	echo Creating \'$mount_file\'.
 	cp $1 $mount_file
 	dd if=/dev/zero bs=$block_size seek=$blocks count=$patch_blocks of=$mount_file &> /dev/null
 
-	echo Fixing the ext4 image file \'$mount_file\' using fsck.ext4
+	echo Fixing the ext4 image file \'$mount_file\' using fsck.ext4.
 	fsck.ext4 $mount_file -y &> /dev/null
 fi
 
-echo Mount \'$mount_file\' to \'$2\' directory
+echo Mount \'$mount_file\' to \'$2\' directory.
 mount $mount_file $2
